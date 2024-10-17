3e8abd4e445972a8b0afe7783649b74a6396e528
soumyaponnagani <sowmyaponnagani@gmail.com>
2024-10-17 16:07:56 +0530

    Add files via upload
    my first file

---
+++ test.js
@@ -0,0 +1,28 @@
+console.log("welcome to java code")
+    let a=20,b=10;
+    if(a>b)
+    {
+        console.log("a is greater")
+    }
+    else if(a<b){
+        console.log("b is greater")
+    }
+    <!DOCTYPE html>
+    <html>
+    <body>
+    
+    <p id="demo">Click the button to change the layout of this paragraph</p>
+    
+    <button onclick="myFunction()">Click Me!</button>
+    
+    <script>
+    function myFunction() {
+      let x = document.getElementById("demo");
+      x.style.fontSize = "25px"; 
+      x.style.color = "red"; 
+    }
+    </script>
+    
+    </body>
+    </html>
+    
